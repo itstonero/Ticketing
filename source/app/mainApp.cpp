0fbc8f4dec4565748583955821d8d100d3eaa7bd
Asadu Anthony <asadu.anthony@outlook.com>
2020-05-05 19:34:13 +0100

    Working on mySQL shared Lib

--- source/app/mainApp.cpp
+++ source/app/mainApp.cpp
@@ -1,8 +1,20 @@
+#include <mysqlx/xdevapi.h>
 #include <iostream>
 
+// Scope controls life-time of objects such as session or schema
+using namespace ::mysqlx;
 
 int main(){
+  Session sess("127.0.0.1", 3306, "root", "benedict");
+  Schema db= sess.getSchema("tonero");
+  // or Schema db(sess, "test");
 
-  std::cout << "Application is starting Up ... " << std::endl;
+  Collection myColl = db.getCollection("appuserroles");
+  // or Collection myColl(db, "my_collection");
+
+  DocResult myDocs = myColl.find("Name like :param").limit(1).bind("param","L%").execute();
+
+  std::cout << myDocs.fetchOne();
+
   return 0;
 }
